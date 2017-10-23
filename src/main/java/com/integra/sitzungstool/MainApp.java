ec678241fe0759078c75e208d06150f423759e84
Marius <marius@uhab.de>
2017-10-23 17:38:42 +0200

    Merge branch master into http

--- src/main/java/com/integra/sitzungstool/MainApp.java
+++ src/main/java/com/integra/sitzungstool/MainApp.java
@@ -1,32 +1,67 @@
 package com.integra.sitzungstool;
 
 import com.integra.sitzungstool.controller.MainViewController;
+import java.util.Optional;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class MainApp extends Application
 {
     @Override
     public void start(Stage mainStage) throws Exception
-    {
-        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
-        
+    {   
         FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/MainViewRessort.fxml"));
 	Parent geparsteFxmlFile = loader.load();
 	Scene mainScene = new Scene(geparsteFxmlFile);
 	mainStage.setScene(mainScene);
-		
+        
+        //Controller initialisieren       
         MainViewController mvc = (MainViewController) loader.getController();
 	mvc.init();
-		
+        
+        
+        //Wirklich beenden Dialog hinzufügen
+        mainStage.setOnCloseRequest((WindowEvent event) ->
+        {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.initOwner(mainStage);
+                alert.initModality(Modality.WINDOW_MODAL);
+                alert.setTitle("Wirklich beenden?");
+                alert.setHeaderText("Wirklich beenden?");
+                alert.setContentText("Sind Sie sicher, dass Sie das Sitzungstool schließen wollen?");
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (!(result.get() == ButtonType.OK))
+                {
+                        //Schließen abbrechen
+                        event.consume();
+                }
+            });
+	
+        mainScene.setOnKeyReleased((KeyEvent keyEvent) -> {
+            if("ESCAPE".equals(keyEvent.getCode().name()))
+            {
+                mvc.clearTextField();
+            }
+            keyEvent.consume();
+        });
+        
+        //Titel hinzufügen
+	mainStage.setTitle("INTEGRA Sitzungstool");
+
 	mainStage.show();
-                
+        mvc.showLoginPopup();
     }
     
     public static void main(String[] args)
--- src/main/java/com/integra/sitzungstool/model/Integraner.java
+++ src/main/java/com/integra/sitzungstool/model/Integraner.java
@@ -2,33 +2,93 @@ package com.integra.sitzungstool.model;
 
 import java.util.Arrays;
 import org.apache.commons.lang3.text.WordUtils;
+import javafx.scene.image.Image;
 
-public class Integraner {
-    private String id;
-    private String ressort;
-    private String stab;
-    private boolean anwesend = false;
-    
-    public String getId() {
-        return this.id;
-    }
-    
-    public String getName() {
-        /* String[] names = this.id.split(".");
-        String name = names[0] + " " + names[1];
-        return WordUtils.capitalize(name); */
-        return this.id;
-    }
-    
-    public String getRessort() {
-        return this.ressort;
-    }
-    
-    public boolean getAnwesend() {
-        return this.anwesend;
-    }
-    
-    public void setAnwesend(boolean anwesend) {
-        this.anwesend = anwesend;
-    }
+public class Integraner
+{
+	private String benutzerkennung;
+	private Image bild;
+	private String name;
+	private String ressort;
+        private String stab;
+	private boolean anwesend;
+	
+	public Integraner(String benutzerkennung, Image bild, String name, String ressort, String stab)
+	{
+		this.benutzerkennung = benutzerkennung;
+		this.bild = bild;
+		this.name = name;
+		this.ressort = ressort;
+                this.stab = stab;
+		
+		this.anwesend = false;
+	}
+	
+	public String toString()
+	{
+		return benutzerkennung;
+	}
+
+	public String getBenutzerkennung()
+	{
+		return this.benutzerkennung;
+	}
+
+	public void setBenutzerkennung(String benutzerkennung)
+	{
+		this.benutzerkennung = benutzerkennung;
+	}
+
+	public Image getBild()
+	{
+		return this.bild;
+	}
+
+	public void setBild(Image bild)
+	{
+		this.bild = bild;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getRessort()
+	{
+		return this.ressort;
+	}
+
+	public void setRessort(String ressort)
+	{
+		this.ressort = ressort;
+	}
+        
+        public String getStab()
+	{
+		return this.stab;
+	}
+
+	public void setStab(String stab)
+	{
+		this.stab = stab;
+	}
+
+	
+	public boolean isAnwesend()
+	{
+		return this.anwesend;
+	}
+
+	public void setAnwesend(boolean anwesend)
+	{
+		this.anwesend = anwesend;
+	}
+	
+	
 }
